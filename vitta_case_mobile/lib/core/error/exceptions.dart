2710ccfa55e0b0e9a154f65efde4346559ef91f0
fellipedm <fellipe@engapp.com.br>
2020-02-19 20:31:34 -0300

    Added on more test and added a ServerFailure object

--- vitta_case_mobile/lib/core/error/exceptions.dart
+++ vitta_case_mobile/lib/core/error/exceptions.dart
@@ -1,1 +1,8 @@
+import 'package:vitta_case_mobile/core/error/failure.dart';
+
 class ServerException implements Exception {}
+
+class ServerFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
--- vitta_case_mobile/test/features/movies/domain/use_cases/search_movie_test.dart
+++ vitta_case_mobile/test/features/movies/domain/use_cases/search_movie_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:vitta_case_mobile/core/error/exceptions.dart';
 import 'package:vitta_case_mobile/features/movies/domain/entities/movie_object.dart';
 import 'package:vitta_case_mobile/features/movies/domain/repositories/movies_repository.dart';
 import 'package:vitta_case_mobile/features/movies/domain/use_cases/get_searched_movies.dart';
@@ -25,15 +26,29 @@ void main() {
             'https://m.media-amazon.com/images/M/MV5BZmUwNGU2ZmItMmRiNC00MjhlLTg5YWUtODMyNzkxODYzMmZlXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg',
         year: '2005')
   ];
-  test('should get all movies when requested', () async {
-    when(mockMoviesRepository.searchedMovie(any))
-        .thenAnswer((_) async => Right(tMovie));
+  group('Searched Movies', () {
+    test('should get all movies when requested with [$tQuery]', () async {
+      when(mockMoviesRepository.searchedMovie(any))
+          .thenAnswer((_) async => Right(tMovie));
 
-    final result = await useCase(SearchMovieParams(query: tQuery));
+      final result = await useCase(SearchMovieParams(query: tQuery));
 
-    expect(result, Right(tMovie));
+      expect(result, Right(tMovie));
 
-    verify(mockMoviesRepository.searchedMovie(tQuery));
-    verifyNoMoreInteractions(mockMoviesRepository);
+      verify(mockMoviesRepository.searchedMovie(tQuery));
+      verifyNoMoreInteractions(mockMoviesRepository);
+    });
+
+    test('should get no movies when requested with empty query', () async {
+      when(mockMoviesRepository.searchedMovie(''))
+          .thenAnswer((_) async => Left(ServerFailure()));
+
+      final result = await useCase(SearchMovieParams(query: ''));
+
+      expect(result, Left(ServerFailure()));
+
+      verify(mockMoviesRepository.searchedMovie(''));
+      verifyNoMoreInteractions(mockMoviesRepository);
+    });
   });
 }
